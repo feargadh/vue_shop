ee1f5215d9d8afb3271585e28cfedeadd7a82351
feargadh <602046764@qq.com>
2020-07-14 18:10:25 +0800

    修改baseURL

--- src/main-prod.js
+++ src/main-prod.js
@@ -12,7 +12,7 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
-axios.defaults.baseURL='http://timemeetyou.com:8889/api/private/v1/'
+axios.defaults.baseURL='https://timemeetyou.com:8889/api/private/v1/'
 axios.interceptors.request.use(config => {
   console.log(config);
   config.headers.Authorization = window.sessionStorage.getItem('token')
